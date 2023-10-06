6fb28ea5a8ad9745b29998d2647bc4da01212232
Andrew Herder <andrew.herder@gmail.com>
2023-10-06 14:46:40 -0500

    permissions on file

--- mtga-opcache-preload/provision.sh
+++ mtga-opcache-preload/provision.sh
@@ -4,9 +4,10 @@
 echo " * Checking for opcache preload"
 if [[ ! -f "/etc/php/8.2/fpm/conf.d/20-opcache.ini" ]]; then
   sudo touch /etc/php/8.2/fpm/conf.d/20-opcache.ini
-  "opcache.enable=1" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
-  "opcache.preload_user=www-data" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
-  "opcache.preload=/srv/www/mtga/public_html/preload.php" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
+  sudo chmod 777 /etc/php/8.2/fpm/conf.d/20-opcache.ini
+  sudo "opcache.enable=1" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
+  sudo "opcache.preload_user=www-data" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
+  sudo "opcache.preload=/srv/www/mtga/public_html/preload.php" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
 else
 	echo " * opcache preload already set up."
 fi
