4dc2ae777b41d4634c9bfad371964f52fef60467
Andrew Herder <andrew.herder@gmail.com>
2023-10-06 15:02:10 -0500

    restart php after changes

--- memcached-php-82/provision.sh
+++ memcached-php-82/provision.sh
@@ -6,6 +6,7 @@ if [[ ! -f "/usr/bin/memcached" ]]; then
 	echo -e " * Downloading memcached, see https://www.php.net/manual/en/book.memcached.php"
 	sudo apt-get install memcached
 	sudo apt-get install php-memcached
+  sudo systemctl restart php8.2-fpm
 else
 	echo " * memcached already installed."
 fi
--- mtga-opcache-preload/provision.sh
+++ mtga-opcache-preload/provision.sh
@@ -8,6 +8,7 @@ if [[ ! -f "/etc/php/8.2/fpm/conf.d/20-opcache.ini" ]]; then
   sudo echo "opcache.enable=1" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
   sudo echo "opcache.preload_user=www-data" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
   sudo echo "opcache.preload=/srv/www/mtga/public_html/preload.php" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
+  sudo systemctl restart php8.2-fpm
 else
 	echo " * opcache preload already set up."
 fi
