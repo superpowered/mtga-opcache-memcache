121a2d2feb75e7db829717787f803a2aa25362a2
Andrew Herder <andrew.herder@gmail.com>
2024-09-02 12:17:39 -0500

    Update provision.sh

--- mtga-opcache-preload/provision.sh
+++ mtga-opcache-preload/provision.sh
@@ -7,7 +7,7 @@ if [[ ! -f "/etc/php/8.2/fpm/conf.d/20-opcache.ini" ]]; then
   sudo chmod 777 /etc/php/8.2/fpm/conf.d/20-opcache.ini
   sudo echo "opcache.enable=1" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
   sudo echo "opcache.preload_user=www-data" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
-  sudo echo "opcache.preload=/srv/www/mtga/public_html/preload.php" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
+  sudo echo "opcache.preload=/srv/www/mtga_new/preload.php" >> /etc/php/8.2/fpm/conf.d/20-opcache.ini
   sudo systemctl restart php8.2-fpm
 else
 	echo " * opcache preload already set up."
